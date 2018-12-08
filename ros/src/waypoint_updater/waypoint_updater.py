e514c39b7aea068a78c3d0e4c5a84c322f606471
Karthik N <karthik.chopper@gmail.com>
2018-12-08 14:12:20 -0800

    Went through the entire track till the waypoints got over

--- ros/src/waypoint_updater/waypoint_updater.py
+++ ros/src/waypoint_updater/waypoint_updater.py
@@ -52,7 +52,7 @@ class WaypointUpdater(object):
         self.loop()
 
     def loop(self):
-        rate = rospy.Rate(50)
+        rate = rospy.Rate(10)
         while not rospy.is_shutdown():
             if self.pose and self.base_waypoints:
                 # Get the closest waypoints
